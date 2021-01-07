7f296bed3fe75b9898f5bfe89acc6fa152f04387
Chris J. Karr <chris@audacious-software.com>
2021-01-07 20:33:05 +0000

    Pylint code cleanup

--- examples/raspi-1button-2texters/tester.py
+++ examples/raspi-1button-2texters/tester.py
@@ -1,12 +1,8 @@
-import logging
-import sys
-import time
-import client
-import settings
+# pylint: disable=line-too-long, missing-module-docstring
+
 import datetime
 
-from client import *
-from tkinter import *
+from .client import HiveClient, TriggerInterruptCommand, SetVariableCommand, VariableScope
 
 #Swap in the correct Hive API URL and Client Token for your game here:
 HIVE_API_URL = 'https://workshop.hivemechanic.org/http/'
@@ -14,29 +10,33 @@ HIVE_CLIENT_TOKEN = 'abc12345' # nosec - This is a placeholder for someone to fi
 
 
 #Determine what happens when the button is pressed
-def SendMsgClicked():
-        print("Button Pressed")
+def send_msg_clicked():
+    print("Button Pressed")
 
-        #DO NOT TOUCH -- this connects your button to Hive Mechanic
-        client = HiveClient(api_url= HIVE_API_URL, token= HIVE_CLIENT_TOKEN)
+    #DO NOT TOUCH -- this connects your button to Hive Mechanic
+    client = HiveClient(api_url= HIVE_API_URL, token= HIVE_CLIENT_TOKEN)
 
-        commands = [
-            TriggerInterruptCommand('BUTTON-PRESSED'),
-            SetVariableCommand('button_press_ts', datetime.timezone, VariableScope.game)
-                ]
+    commands = [
+        TriggerInterruptCommand('BUTTON-PRESSED'),
+        SetVariableCommand('button_press_ts', datetime.timezone, VariableScope.game)
+    ]
 
-        response = client.issue_command(TriggerInterruptCommand('BUTTON-PRESSED'), player='pi:12345')
+    response = client.issue_command(TriggerInterruptCommand('BUTTON-PRESSED'), player='pi:12345')
 
-        audio_url = client.fetch_variable('claimed_audio_file', scope=VariableScope.game)
+    audio_url = client.fetch_variable('claimed_audio_file', scope=VariableScope.game)
 
-        
-#UI for window + button, using the Python Tkinter library
-window=Tk()
-lbl=Label(window, text="Welcome to the Knock-Knock Game!", fg='red', font=("Helvetica", 16))
-lbl.place(x=125, y=100)
-btn=Button(window, text="Press to Send Message", fg='blue', command=SendMsgClicked)
-btn.place(x=175, y=175)
+try:
+    from tkinter import Tk, Label, Button
 
-window.title('Knock-Knock Game Button')
-window.geometry("500x300+400+275")
-window.mainloop()
+    #UI for window + button, using the Python Tkinter library
+    window=Tk()
+    lbl=Label(window, text="Welcome to the Knock-Knock Game!", fg='red', font=("Helvetica", 16))
+    lbl.place(x=125, y=100)
+    btn=Button(window, text="Press to Send Message", fg='blue', command=send_msg_clicked)
+    btn.place(x=175, y=175)
+
+    window.title('Knock-Knock Game Button')
+    window.geometry("500x300+400+275")
+    window.mainloop()
+except ImportError:
+    print('TK not installed - please implement alternative UI')
