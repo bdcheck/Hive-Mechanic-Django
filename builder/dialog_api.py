9cab2b9f5cdf4f7e905a5d7d552e5055bd4a0fc0
Chris J. Karr <chris@audacious-software.com>
2021-08-17 15:27:34 +0000

    Initial Botium testing support

---
+++ builder/dialog_api.py
@@ -0,0 +1,38 @@
+# pylint: disable=line-too-long, no-member
+
+import json
+import os
+import traceback
+
+from django.utils import timezone
+from django.utils.text import slugify
+
+from django_dialog_engine.models import Dialog
+
+from .models import Game, GameVersion
+
+
+def create_dialog_from_path(file_path):
+    try:
+        base_name = os.path.basename(os.path.normpath(file_path))
+
+        game_slug = slugify(base_name)
+
+        game = Game.objects.filter(slug=game_slug).first()
+
+        if game is None:
+            game = Game.objects.create(slug=game_slug, name=base_name + ' Botium Test Game')
+
+        definition = json.load(open(file_path))
+
+        new_version = GameVersion.objects.create(game=game, created=timezone.now(), definition=json.dumps(definition, indent=2))
+
+        dialog_snapshot = new_version.dialog_snapshot()
+
+        new_dialog = Dialog.objects.create(key=game_slug, dialog_snapshot=dialog_snapshot, started=timezone.now())
+
+        return new_dialog
+    except: # pylint: disable=bare-except
+        traceback.print_exc()
+
+    return None
