c6ef524d70a021efbd9d936fe3ad6e3185ebba5f
Chris J. Karr <chris@audacious-software.com>
2021-08-18 20:41:52 +0000

    Code cleanup

--- builder/tests/tests_browser.py
+++ builder/tests/tests_browser.py
@@ -1,20 +1,12 @@
 # pylint: disable=line-too-long, no-member
 # -*- coding: utf-8 -*-
 
-import json
-
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Group
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.core.management import call_command
-from django.test import TestCase
-from django.urls import reverse
-
-from ..models import Game, InteractionCard
 
 class BasicBrowserTests(StaticLiveServerTestCase):
     @classmethod
@@ -27,7 +19,7 @@ class BasicBrowserTests(StaticLiveServerTestCase):
         cls.selenium = webdriver.Firefox(options=options)
         cls.selenium.implicitly_wait(10)
 
-        user = get_user_model().objects.create_user(username='selenium', email='selenium@example.com', password='browsertesting', is_superuser=True)
+        get_user_model().objects.create_user(username='selenium', email='selenium@example.com', password='browsertesting', is_superuser=True)
 
     @classmethod
     def tearDownClass(cls):
