e72bb2dde2f62c983cd554ecdf096939df0fb430
Chris J. Karr <chris@audacious-software.com>
2021-09-03 03:57:35 +0000

    Dialog API updates

--- builder/dialog_api.py
+++ builder/dialog_api.py
@@ -1,7 +1,10 @@
 # pylint: disable=line-too-long, no-member
 
+from __future__ import print_function
+
 import json
 import os
+import sys
 import traceback
 
 from django.utils import timezone
@@ -9,10 +12,12 @@ from django.utils.text import slugify
 
 from django_dialog_engine.models import Dialog
 
-from .models import Game, GameVersion
+from integrations.models import Integration
+
+from .models import Game, GameVersion, Player, Session
 
 
-def create_dialog_from_path(file_path):
+def create_dialog_from_path(file_path, dialog_key=None):
     try:
         definition = json.load(open(file_path))
 
@@ -21,11 +26,13 @@ def create_dialog_from_path(file_path):
 
             game_slug = slugify(base_name)
 
+            if dialog_key is not None:
+                game_slug = dialog_key
+
             game = Game.objects.filter(slug=game_slug).first()
 
             if game is None:
                 game = Game.objects.create(slug=game_slug, name=base_name + ' Botium Test Game')
-
 
             new_version = GameVersion.objects.create(game=game, created=timezone.now(), definition=json.dumps(definition, indent=2))
 
@@ -38,3 +45,29 @@ def create_dialog_from_path(file_path):
         traceback.print_exc()
 
     return None
+
+def process(dialog, response, extras):
+    game = Game.objects.filter(slug=dialog.key).first()
+
+    integration = Integration.objects.filter(game=game).first()
+
+    if integration is None:
+        integration = Integration.objects.create(url_slug=dialog.key, name=dialog.key + ' Botium Integration', type='other', game=game)
+
+    player_match = Player.objects.filter(identifier=extras['player']).first()
+
+    if player_match is None:
+        player_match = Player.objects.create(identifier=extras['player'], player_state=extras)
+
+    session = game.current_active_session(player=player_match)
+
+    if session is None:
+        session = Session(game_version=game.versions.order_by('-created').first(), player=player_match, started=timezone.now())
+        session.save()
+
+        if extras is not None and 'last_message' in extras:
+            del extras['last_message']
+
+    print('PROCESSING ' + str(response), file=sys.stderr)
+    session.process_incoming(integration, response, extras, dialog=dialog)
+    print('PROCESSED ' + str(response), file=sys.stderr)
