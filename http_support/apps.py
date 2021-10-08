f7daeddf0d3bc4a41d1caa1c7d5979d6963d0974
Chris J. Karr <chris@audacious-software.com>
2021-10-08 14:12:15 +0000

    Explicitly specifiying ID field type for Django 3

--- http_support/apps.py
+++ http_support/apps.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-
 from django.apps import AppConfig
 
 
 class HttpSupportConfig(AppConfig):
     name = 'http_support'
     verbose_name = 'HTTP Integration Support'
+    default_auto_field = 'django.db.models.AutoField'
--- twilio_support/apps.py
+++ twilio_support/apps.py
@@ -7,3 +7,4 @@ from django.apps import AppConfig
 class TwilioSupportConfig(AppConfig):
     name = 'twilio_support'
     verbose_name = 'Twilio Integration Support'
+    default_auto_field = 'django.db.models.AutoField'
