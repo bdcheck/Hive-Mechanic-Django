358917c8f055974d52d1562dce69ceb3d086b35a
Chris J. Karr <chris@audacious-software.com>
2022-02-13 22:57:13 +0000

    Updating Selenium tests to accomodate UI changes in builder

--- builder/tests/tests_browser_empty_card_ids.py
+++ builder/tests/tests_browser_empty_card_ids.py
@@ -88,6 +88,8 @@ class BrowserEmptyCardIdTests(StaticLiveServerTestCase):
 
             WebDriverWait(self.selenium, 15).until(lambda driver: driver.find_element_by_xpath('//div[@data-node-id="response-test-2"]'))
 
+            WebDriverWait(self.selenium, 5).until(lambda driver: driver.find_element_by_class_name('mdc-top-app-bar__navigation-icon'))
+
             self.selenium.find_element_by_class_name('mdc-top-app-bar__navigation-icon').click()
 
             next_nodes.find_element_by_xpath('//div[@data-node-id="response-test-2"]').click()
