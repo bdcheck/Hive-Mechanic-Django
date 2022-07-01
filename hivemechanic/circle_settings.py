0619e6095a2a40e8ac0715db24a7d3c122c0c239
Chris Karr <chris@audacious-software.com>
2022-07-01 10:29:54 -0500

    Update circle_settings.py
    Security updates for CI builds.

--- hivemechanic/circle_settings.py
+++ hivemechanic/circle_settings.py
@@ -3,9 +3,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'CHANGEME' # nosec
+SECRET_KEY = 'CHANGEMECHANGEMECHANGEMECHANGEMECHANGEMECHANGEMECHANGEMECHANGEMECHANGEMECHANGEME' # nosec
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
 	'dev.hivemechanic.org'
