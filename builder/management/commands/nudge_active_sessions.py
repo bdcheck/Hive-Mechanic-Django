33776b56a9f07e6f2f570a739c334ed8446374bf
Chris J. Karr <chris@audacious-software.com>
2022-09-29 21:26:05 +0000

    Dependency and testing update

--- builder/management/commands/nudge_active_sessions.py
+++ builder/management/commands/nudge_active_sessions.py
@@ -1,12 +1,17 @@
 # pylint: disable=no-member, line-too-long
 
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 from quicksilver.decorators import handle_lock, handle_schedule, add_qs_arguments
 
 from cli_support.models import HiveActivityFinishedException
 
 from ...models import Session
+
+start = timezone.now()
+
+print('0: %s' % start.isoformat())
 
 class Command(BaseCommand):
     @add_qs_arguments
@@ -16,6 +21,10 @@ class Command(BaseCommand):
     @handle_lock
     @handle_schedule
     def handle(self, *args, **cmd_options): # pylint: disable=unused-argument
+        here = timezone.now()
+
+        print('1: %s -- %s' % (here.isoformat(), (here - start)))
+
         for session in Session.objects.filter(completed=None):
             if 'is_testing' in session.session_state and session.session_state['is_testing'] is True:
                 pass # Skip - this is a test session
@@ -24,3 +33,7 @@ class Command(BaseCommand):
                     session.nudge()
                 except HiveActivityFinishedException:
                     pass
+
+        here = timezone.now()
+
+        print('2: %s -- %s' % (here.isoformat(), (here - start)))
