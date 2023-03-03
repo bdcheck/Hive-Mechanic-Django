4662743b0732c1ad86274760bbeedb64b71f24d4
Chris J. Karr <chris@audacious-software.com>
2023-03-03 02:47:53 +0000

    Code cleanup

--- activity_logger/urls.py
+++ activity_logger/urls.py
@@ -3,6 +3,6 @@
 # from .views import incoming_twilio, incoming_twilio_call
 
 urlpatterns = [
-#    url(r'^incoming.xml$', incoming_twilio, name='incoming_twilio'),
-#    url(r'^call-incoming.xml$', incoming_twilio_call, name='incoming_twilio_call'),
+    # url(r'^incoming.xml$', incoming_twilio, name='incoming_twilio'),
+    # url(r'^call-incoming.xml$', incoming_twilio_call, name='incoming_twilio_call'),
 ]
