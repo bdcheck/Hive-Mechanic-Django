acbf82cb41a8ab543a6c75229af744202997fc68
Chris J. Karr <chris@audacious-software.com>
2023-04-14 16:03:04 +0000

    JS code cleanup

--- builder/static/builder-js/js/app/activity_log_table.js
+++ builder/static/builder-js/js/app/activity_log_table.js
@@ -56,112 +56,111 @@ requirejs(['material', 'cookie', 'jquery'], function (mdc, Cookies) {
   })
 
   drawer.open = true
-  
+
   const itemsPerPage = mdc.select.MDCSelect.attachTo(document.getElementById('items_per_page'))
-  
+
   const currentUrl = new URL(window.location)
-  
+
   let pageSize = currentUrl.searchParams.get('size')
-  
+
   if (pageSize === undefined || pageSize === null) {
-  	pageSize = '25'
+    pageSize = '25'
   }
-  
+
   itemsPerPage.value = pageSize
 
   itemsPerPage.listen('MDCSelect:change', function () {
-  	const currentUrl = new URL(window.location)
-  	
-  	currentUrl.searchParams.set('size', itemsPerPage.value)
-  	
-  	window.location = currentUrl.href
-  });
+    const currentUrl = new URL(window.location)
 
-  const dataTable = mdc.dataTable.MDCDataTable.attachTo(document.getElementById('events_table'))
-  
-  $('.mdc-data-table__row').click(function(event) {
-  	let target = $(event.target)
-  	
-  	while (target.get(0).nodeName.toLowerCase() !== 'tr') {
-  		target = target.parent()
-  	}
-  	
-  	$('#event_name').html(target.data('name'))
-  	$('#event_summary').html(target.data('message'))
-  	$('#event_date').html(target.data('date'))
-  	
-  	console.log('PARSE: ')
-  	console.log(target.data('details'))
-  	
-  	const details = target.data('details')
-  	
-  	let detailsHtml = ''
+    currentUrl.searchParams.set('size', itemsPerPage.value)
 
-  	for (let key of Object.keys(details)) {
-  		let value = details[key]
-  		
-  		if (detailsHtml !== '') {
-  			detailsHtml += '<br />'
-  		}
-  		
-  		if (key === 'hive_player') {
-  			key = 'Player'
-  			
-			value = 'twilio_player:XXXXXX' + value.slice(-4)
-  		} else if (key === 'hive_session') {
-  			key = 'Session'
-  		} else if (key === 'game_version') {
-  			key = 'Game'
-  		}
+    window.location = currentUrl.href
+  })
 
-		detailsHtml += '<strong>' + key + ':</strong> ' + value
-  	}
- 
-   	$('#event_details').html(detailsHtml)
-   	$('#event_preview').hide()
+  mdc.dataTable.MDCDataTable.attachTo(document.getElementById('events_table'))
+
+  $('.mdc-data-table__row').click(function (event) {
+    let target = $(event.target)
+
+    while (target.get(0).nodeName.toLowerCase() !== 'tr') {
+      target = target.parent()
+    }
+
+    $('#event_name').html(target.data('name'))
+    $('#event_summary').html(target.data('message'))
+    $('#event_date').html(target.data('date'))
+
+    console.log('PARSE: ')
+    console.log(target.data('details'))
+
+    const details = target.data('details')
+
+    let detailsHtml = ''
+
+    for (let key of Object.keys(details)) {
+      let value = details[key]
+
+      if (detailsHtml !== '') {
+        detailsHtml += '<br />'
+      }
+
+      if (key === 'hive_player') {
+        key = 'Player'
+
+        value = 'twilio_player:XXXXXX' + value.slice(-4)
+      } else if (key === 'hive_session') {
+        key = 'Session'
+      } else if (key === 'game_version') {
+        key = 'Game'
+      }
+
+      detailsHtml += '<strong>' + key + ':</strong> ' + value
+    }
+
+    $('#event_details').html(detailsHtml)
+    $('#event_preview').hide()
 
     const dialogPreview = mdc.dialog.MDCDialog.attachTo(document.getElementById('dialog_preview'))
     console.log('dialogPreview')
     console.log(dialogPreview)
 
-  	const preview = target.data('attachment')
-  	
-  	console.log('PREVIEW: ')
-  	console.log(preview)
+    const preview = target.data('attachment')
 
-  	if (preview) {
-	   	$('#event_preview').attr('src', preview)
-	   	$('#event_preview').show()
+    console.log('PREVIEW: ')
+    console.log(preview)
 
-	   	$('#dialog_preview_image').attr('src', preview)
-	} else {
-	   	$('#event_preview').hide()
-	}
+    if (preview) {
+      $('#event_preview').attr('src', preview)
+      $('#event_preview').show()
 
-	$('#event_preview').click(function() {
-		console.log('CLICK PREVIEW')
-		dialogPreview.open()
-	})
- 	
-  	const currentBackground = target.css('background-color')
-  	
-  	$('.mdc-data-table__row').css('background-color', '')
-  	
-  	if (currentBackground === 'rgb(224, 224, 224)' ) { // Deselect
-	  $('#event_details_view').hide()
-	  $('#empty_details').show()
-  	} else { // Select
-	  $('#event_details_view').show()
-	  $('#empty_details').hide()
+      $('#dialog_preview_image').attr('src', preview)
+    } else {
+      $('#event_preview').hide()
+    }
+
+    $('#event_preview').click(function () {
+      console.log('CLICK PREVIEW')
+      dialogPreview.open()
+    })
+
+    const currentBackground = target.css('background-color')
+
+    $('.mdc-data-table__row').css('background-color', '')
+
+    if (currentBackground === 'rgb(224, 224, 224)') { // Deselect
+      $('#event_details_view').hide()
+      $('#empty_details').show()
+    } else { // Select
+      $('#event_details_view').show()
+      $('#empty_details').hide()
 
       target.css('background-color', 'rgb(224, 224, 224)')
-  	} 
+    }
   })
 
   $('#event_details_view').hide()
 
-
-  const startDate = mdc.textField.MDCTextField.attachTo(document.getElementById('start_date'))
-  const endDate = mdc.textField.MDCTextField.attachTo(document.getElementById('end_date'))
-  const searchField = mdc.textField.MDCTextField.attachTo(document.getElementById('search_field'))
+  mdc.textField.MDCTextField.attachTo(document.getElementById('start_date'))
+  mdc.textField.MDCTextField.attachTo(document.getElementById('end_date'))
+  mdc.textField.MDCTextField.attachTo(document.getElementById('search_field'))
 })
