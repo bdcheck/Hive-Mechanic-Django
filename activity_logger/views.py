6a62530d69b6d81ff1ad5a5841ed0c1f5393c55b
Chris J. Karr <chris@audacious-software.com>
2023-06-14 14:01:57 +0000

    Adding guard to prevent error logger from generating new errors itself

--- activity_logger/views.py
+++ activity_logger/views.py
@@ -10,48 +10,51 @@ from django.utils import timezone
 from .models import LogItem, LogTag
 
 def log_500_error(request, template_name='500.html'): # pylint: disable=too-many-locals
-    tag = LogTag.objects.filter(tag='server-error').first()
+    try: # Added to prevent any endless loops caused by issues in the code below...
+        tag = LogTag.objects.filter(tag='server-error').first()
 
-    if tag is None:
-        tag = LogTag.objects.create(tag='server-error', name='Server Error')
+        if tag is None:
+            tag = LogTag.objects.create(tag='server-error', name='Server Error')
 
-    source = 'path:%s' % request.path
+        source = 'path:%s' % request.path
 
-    exception_type, exception, exception_traceback = sys.exc_info()
-    message = str(exception)
+        exception_type, exception, exception_traceback = sys.exc_info()
+        message = str(exception)
 
-    get_dict = {}
-    get_dict.update(request.GET)
+        get_dict = {}
+        get_dict.update(request.GET)
 
-    post_dict = {}
-    post_dict.update(request.POST)
+        post_dict = {}
+        post_dict.update(request.POST)
 
-    meta_dict = {}
+        meta_dict = {}
 
-    for key in request.META:
-        meta_dict[key] = str(request.META[key])
+        for key in request.META:
+            meta_dict[key] = str(request.META[key])
 
-    headers_dict = {}
+        headers_dict = {}
 
-    for header in request.headers:
-        headers_dict[header] = str(request.headers[header])
+        for header in request.headers:
+            headers_dict[header] = str(request.headers[header])
 
-    metadata = {
-        'traceback': traceback.format_exception(exception_type, exception, exception_traceback),
-        'path': request.path,
-        'method': request.method,
-        'GET': get_dict,
-        'POST': post_dict,
-        'META': meta_dict,
-        'HEADERS': headers_dict,
-    }
+        metadata = {
+            'traceback': traceback.format_exception(exception_type, exception, exception_traceback),
+            'path': request.path,
+            'method': request.method,
+            'GET': get_dict,
+            'POST': post_dict,
+            'META': meta_dict,
+            'HEADERS': headers_dict,
+        }
 
-    log_item = LogItem(source=source, message=message, logged=timezone.now())
-    log_item.metadata = json.dumps(metadata, indent=2)
+        log_item = LogItem(source=source, message=message, logged=timezone.now())
+        log_item.metadata = json.dumps(metadata, indent=2)
 
-    log_item.save()
+        log_item.save()
 
-    log_item.tags.add(tag)
+        log_item.tags.add(tag)
+    except: # pylint: disable=bare-except
+        traceback.print_exc()
 
     return server_error(request, template_name)
 
