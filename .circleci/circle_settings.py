a4d95a894334d0939155da0fbf313840cd2aaa23
Chris Karr <github@audacious-software.com>
2025-03-07 10:34:40 -0600

    CircleCI updates

--- .circleci/circle_settings.py
+++ .circleci/circle_settings.py
@@ -50,3 +50,5 @@ HIVE_CLIENT_TOKEN = 'abc12345' # nosec
 # TEST_RUNNER = 'hivemechanic.no_db_test_runner.NoDbTestRunner'
 
 DEFAULT_FROM_MAIL_ADDRESS = 'Hive Mechanic <noreply@hivemechanic.net>'
+
+ADDITIONAL_APPS = []
