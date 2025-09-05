2f0b29c5f540d9400bdebb38175b5f7382ddd6d3
Chris J. Karr <chris@audacious-software.com>
2025-09-05 20:26:24 +0000

    Adding Quicksilver lock directive

--- hivemechanic/settings.py
+++ hivemechanic/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import logging
 import os
 import sys
+import tempfile
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
