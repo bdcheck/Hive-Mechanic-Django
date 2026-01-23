4b4d70f60f35ca9d99c6602ea9f26f15d0608aa3
Chris J. Karr <chris@audacious-software.com>
2026-01-23 18:59:54 +0000

    Bandit code cleanup

--- .circleci/circle_settings.py
+++ .circleci/circle_settings.py
@@ -20,7 +20,7 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME':     'circle_test',
         'USER':     'root',
-        'PASSWORD': '',
+        'PASSWORD': '', # nosec
         'HOST': 'localhost',
     }
 }
